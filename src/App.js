79a1ad60d2a3ed9808c119d180768b14468818b3
Allen Lewis <allenlewis32@gmail.com>
2023-06-01 02:02:54 +0530

    Add. Added dial

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import StopWatch from './components/StopWatchComponent';
 function App() {
   return (
     <div className='main'>
+      <h3>Stop Watch</h3>
       <StopWatch></StopWatch>
     </div>
   );
--- src/components/StopWatchComponent.js
+++ src/components/StopWatchComponent.js
@@ -1,21 +1,46 @@
 import React, { useState } from "react";
 
 let display, setDisplay;
+let seconds, setSeconds;
+let minutes, setMinutes;
 let interval;
-let state = 0; // 0 - stopped, 1 - running, 2 - paused
+let state, setState; // 0 - stopped, 1 - running, 2 - paused
 
 let time = 0;
 
 function StopWatch() {
     [display, setDisplay] = useState('00:00.00');
+    [seconds, setSeconds] = useState(0);
+    [minutes, setMinutes] = useState(0);
+    [state, setState] = useState(0);
+    let secondHandStyle = {
+        'transform':seconds
+    };
+    let minuteHandStyle = {
+        'transform':minutes
+    }
     return (
         <React.Fragment>
-            <div className='stopwatch'>{display}</div>
-            <button type='button' onClick={start}>Start</button>
-            <button type='button' onClick={pause}>Pause</button>
-            <button type='button' onClick={stop}>Stop</button>
-            <button type='button' onClick={reset}>Reset</button>
-            <button type='button'>Lap</button>
+            <div className='dial-second'>
+                <div className='dial-minute'>
+                    <div id='minuteHand' className='hand-minute' style={minuteHandStyle}></div>
+                    <div className='dial-north-minute'>30</div>
+                    <div className='dial-south-minute'>15</div>
+                </div>
+                <div id='secondHand' className='hand-second' style={secondHandStyle}></div>
+                <div className='dial-north'>60</div>
+                <div className='dial-east'>15</div>
+                <div className='dial-south'>30</div>
+                <div className='dial-west'>45</div>
+            </div>
+            {display}
+            <div className='buttons'>
+                {state !== 1 && <button type='button' onClick={start}>Start</button>}
+                {state === 1 && <button type='button' onClick={pause}>Pause</button>}
+                {state !== 0 && <button type='button' onClick={stop}>Stop</button>}
+                {display !== '00:00.00' && <button type='button' onClick={reset}>Reset</button>}
+                <button type='button'>Lap</button>
+            </div>
         </React.Fragment>
     );
 }
@@ -26,7 +51,7 @@ function start() {
             time = 0;
             update();
         }
-        state = 1;
+        setState(1);
         interval = setInterval(() => {
             time += 0.01;
             update();
@@ -36,7 +61,7 @@ function start() {
 
 function pause() {
     if(state === 1) {
-        state = 2;
+        setState(2);
         clearInterval(interval);
     }
 }
@@ -46,7 +71,7 @@ function stop() {
         if(state === 1) {
             clearInterval(interval);
         }
-        state = 0;
+        setState(0);
     }
 }
 
@@ -63,6 +88,9 @@ function update() {
     result += ':' + ('0' + Math.floor(time % 60)).slice(-2);
     result += '.' + ('0' + Math.floor(time % 1 * 100) / 100).slice(-2);
     setDisplay(result);
+
+    setSeconds(`rotate(${time % 60 * 6}deg)`);
+    setMinutes(`rotate(${time / 60 * 12}deg)`);
 }
 
 export default StopWatch;
